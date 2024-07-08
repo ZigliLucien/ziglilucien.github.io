b2161f760abb61353dea4593a67ea8407287912c
ZigliLucien <ziglilechien@gmail.com>
2024-07-08 10:57:01 -0500

    ff

--- WeatherNotes/WeatherNotes.py
+++ WeatherNotes/WeatherNotes.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python3
 
-from json2xml import json2xml
-from json2xml.utils import readfromurl, readfromstring, readfromjson
+import requests
 import json
 
 
-# get the xml from an URL that return json
-data = readfromurl("https://api.weather.gov/gridpoints/PAH/107,82/forecast")
-z = json2xml.Json2xml(data).to_xml()
+data = requests.get("https://api.weather.gov/gridpoints/PAH/107,82/forecast").json()
 
 zout = open("/home/pi/WeatherNotes/fcast.json","w")
 json.dump(data,zout)
 zout.close()
 
-out = open("/home/pi/WeatherNotes/fcast.xml","w")
-out.write(z)
-out.close()
 
-
-#from json2xml import json2xml
-#from json2xml.utils import readfromurl, readfromstring, readfromjson
-
-# get the xml from an URL that return json
-data=readfromurl("https://api.weather.gov/alerts/active?zone=ILZ084")
-z = json2xml.Json2xml(data).to_xml()
+data=requests.get("https://api.weather.gov/alerts/active?zone=ILZ084").json()
 
 zout = open("/home/pi/WeatherNotes/alert.json","w")
 json.dump(data,zout)
 zout.close()
 
-out = open("/home/pi/WeatherNotes/alert.xml","w")
-out.write(z)
-out.close()
 
 
